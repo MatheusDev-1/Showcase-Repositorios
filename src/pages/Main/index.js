9a78094214b643f4f75aa81158428269f5504cdc
Matheus O. Hora <matheus.mdahora@gmail.com>
2020-02-20 07:30:50 -0300

    Primeiros toques

---
+++ src/pages/Main/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Title } from './styles';
+
+// import { Container } from './styles';
+
+export default function Main() {
+  return (
+  <Title>
+    Hello World
+  </Title>
+  )
+}
---
+++ src/pages/Repository/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+export default function pages() {
+  return (
+    <h1>Repository</h1>
+  );
+}
